f56901e81531df18ff09397cbe5407d7b757a1ee
groundswel <36755084+groundswel@users.noreply.github.com>
2023-01-04 20:40:59 -0800

    Update ticker2w.py

--- ticker2w.py
+++ ticker2w.py
@@ -11,8 +11,8 @@ symbol = input("Enter the ticker symbol: ")
 data = yf.Ticker(symbol).history(period="5y")
 
 # Extract the input features and output as numpy arrays
-X = data[["close", "volume"]].values
-y = data["close"].values
+X = data[["Open", "High", "Low", "Close", "Volume"]].values
+y = data[["High", "Low"]].values
 
 # Create a linear regression model
 model = LinearRegression()
@@ -20,12 +20,12 @@ model = LinearRegression()
 # Fit the model to the data
 model.fit(X, y)
 
-# Predict the stock price for the next two weeks
-predictions = model.predict(np.array([[140, 100000], [140, 100000], [140, 100000], [140, 100000], [140, 100000], [140, 100000], [140, 100000]]))
+# Predict the stock high and low prices for the next two weeks
+predictions = model.predict(np.array([[140, 150, 130, 140, 100000], [140, 150, 130, 140, 100000], [140, 150, 130, 140, 100000], [140, 150, 130, 140, 100000], [140, 150, 130, 140, 100000], [140, 150, 130, 140, 100000], [140, 150, 130, 140, 100000]]))
 
-# Calculate the minimum and maximum predicted price
-min_price = min(predictions)
-max_price = max(predictions)
+# Calculate the minimum and maximum predicted prices
+min_price = min(predictions[:, 1])
+max_price = max(predictions[:, 0])
 
 # Print the predicted price range
 print(f"The predicted price range for the next two weeks is ${min_price:.2f} to ${max_price:.2f}")
