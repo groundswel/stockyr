a7f3cd3cc9e54b7d0a3649858c4b022a0d70452a
groundswel <36755084+groundswel@users.noreply.github.com>
2023-01-04 20:33:02 -0800

    Update ticker2w.py

--- ticker2w.py
+++ ticker2w.py
@@ -1,6 +1,7 @@
 # Import libraries
 import pandas as pd
 import yfinance as yf
+import numpy as np
 from sklearn.linear_model import LinearRegression
 
 # Get the ticker symbol from the user
@@ -9,9 +10,9 @@ symbol = input("Enter the ticker symbol: ")
 # Download the stock data from Yahoo Finance
 data = yf.Ticker(symbol).history(period="5y")
 
-# Extract the stock prices as a numpy array
-X = data["Date"].values.reshape(-1, 1)
-y = data["Close"].values
+# Extract the input features and output as numpy arrays
+X = data[["close", "volume"]].values
+y = data["close"].values
 
 # Create a linear regression model
 model = LinearRegression()
@@ -20,7 +21,7 @@ model = LinearRegression()
 model.fit(X, y)
 
 # Predict the stock price for the next two weeks
-predictions = model.predict(np.array([[21], [22], [23], [24], [25], [26], [27]]).reshape(-1, 1))
+predictions = model.predict(np.array([[140, 100000], [140, 100000], [140, 100000], [140, 100000], [140, 100000], [140, 100000], [140, 100000]]))
 
 # Calculate the minimum and maximum predicted price
 min_price = min(predictions)
